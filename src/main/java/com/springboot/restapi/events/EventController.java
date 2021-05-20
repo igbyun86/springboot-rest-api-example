60810b92171074dec16825b005cbbe7111642231
big <igbyun86@gmail.com>
2021-05-20 16:35:20 +0900

    Add events respository

---
+++ src/main/java/com/springboot/restapi/events/EventController.java
@@ -0,0 +1,36 @@
+package com.springboot.restapi.events;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Controller
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
+public class EventController {
+
+    private final EventRepository eventRepository;
+
+    public EventController(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody Event event) {
+        Event newEvent = this.eventRepository.save(event);
+
+
+        URI createdUrl = linkTo(EventController.class).slash(newEvent.getId()).toUri();
+        event.setId(10);
+        return ResponseEntity.created(createdUrl).body(event);
+    }
+
+}
