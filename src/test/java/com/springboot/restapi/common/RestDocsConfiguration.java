90817efd512d6112490e5de4dea288785b89b416
big <igbyun86@gmail.com>
2021-05-27 23:20:17 +0900

    Add rest docs 추가

---
+++ src/test/java/com/springboot/restapi/common/RestDocsConfiguration.java
@@ -0,0 +1,31 @@
+package com.springboot.restapi.common;
+
+import org.springframework.boot.test.autoconfigure.restdocs.RestDocsMockMvcConfigurationCustomizer;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentationConfigurer;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+/**
+ * rest docs 커스텀을 위한 class
+ */
+@TestConfiguration
+public class RestDocsConfiguration {
+
+    /**
+     * request, response json 출력 변경
+     * @return
+     */
+    @Bean
+    public RestDocsMockMvcConfigurationCustomizer restDocsMockMvcConfigurationCustomizer() {
+        return new RestDocsMockMvcConfigurationCustomizer() {
+            @Override
+            public void customize(MockMvcRestDocumentationConfigurer configurer) {
+                configurer.operationPreprocessors()
+                        .withRequestDefaults(prettyPrint())
+                        .withResponseDefaults(prettyPrint());
+            }
+        };
+    }
+}
--- src/test/java/com/springboot/restapi/events/EventControllerTests.java
+++ src/test/java/com/springboot/restapi/events/EventControllerTests.java
@@ -1,13 +1,16 @@
 package com.springboot.restapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.restapi.common.RestDocsConfiguration;
 import com.springboot.restapi.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -15,6 +18,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -22,6 +26,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@Import(RestDocsConfiguration.class)
 public class EventControllerTests {
 
     @Autowired
@@ -64,6 +70,7 @@ public class EventControllerTests {
                 //.andExpect(jsonPath("_links.profile").exists())
                 .andExpect(jsonPath("_links.query-events").exists())
                 .andExpect(jsonPath("_links.update-event").exists())
+                .andDo(document("create-event"))    // create spring rest docs
         ;
     }
 
