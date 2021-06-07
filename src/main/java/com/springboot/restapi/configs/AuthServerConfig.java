3229b5c0d424bc8235f3c20a2d6ec67538e3537c
big <igbyun86@gmail.com>
2021-06-08 00:11:05 +0900

    Add spring security OAuth2인증 서버 설정

---
+++ src/main/java/com/springboot/restapi/configs/AuthServerConfig.java
@@ -0,0 +1,58 @@
+package com.springboot.restapi.configs;
+
+import com.springboot.restapi.accounts.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    TokenStore tokenStore;
+
+    /**
+     * client의 password를 encoding해서 관리
+     * @param security
+     * @throws Exception
+     */
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("myApp")
+                .authorizedGrantTypes("password", "refresh_token")
+                .scopes("read", "write")
+                .secret(this.passwordEncoder.encode("pass"))
+                .accessTokenValiditySeconds(10 * 60)    // 10 minute
+                .refreshTokenValiditySeconds(6 * 10 * 60); // 1 hour
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(accountService)
+                .tokenStore(tokenStore);
+    }
+}
---
+++ src/test/java/com/springboot/restapi/configs/AuthServerConfigTest.java
@@ -0,0 +1,57 @@
+package com.springboot.restapi.configs;
+
+import com.springboot.restapi.accounts.Account;
+import com.springboot.restapi.accounts.AccountRole;
+import com.springboot.restapi.accounts.AccountService;
+import com.springboot.restapi.common.BaseControllerTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class AuthServerConfigTest extends BaseControllerTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Test
+    @DisplayName("인증 토큰을 발급 받는 테스트")
+    public void getAuthToken() throws Exception {
+        String username = "igbyun@email.com";
+        String password = "ig123";
+
+        Set<AccountRole> hs = new HashSet();
+        hs.add(AccountRole.ADMIN);
+        hs.add(AccountRole.USER);
+
+        Account account = Account.builder()
+                .email(username)
+                .password(password)
+                .roles(hs)
+                .build();
+
+        this.accountService.saveAccount(account);
+
+        String clientId = "myApp";
+        String clientSecret = "pass";
+
+        this.mockMvc.perform(post("/oauth/token")
+                    .with(httpBasic(clientId, clientSecret))
+                    .param("username", username)
+                    .param("password", password)
+                    .param("grant_type", "password"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("access_token").exists());
+    }
+
+
+}
